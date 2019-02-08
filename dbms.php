503416139aace969fa497413dbe1097cbdeed496
KumKap <42783781+KumKap@users.noreply.github.com>
2019-02-08 10:34:05 +0530

    Update dbms.php

--- dbms.php
+++ dbms.php
@@ -1,34 +1,34 @@
 <?php
-   $dbhost = 'localhost:3036';
-   $dbuser = 'root';
-   $dbpass = 'rootpassword';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   $param1 = $_POST['user_id'];
-   $param2 = $_POST['user_name'];
+  
+   $conn = mysqli_connect("localhost", "root");
+   $param1 = $_POST['id'];
+   $param2 = $_POST['email'];
    $param3 = 10;
    $param4 = 0;
    if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
+      die('Could not connect: ' . mysqli_error());
    }
    
    
-   $sql = "SELECT user_name, user_score, user_attempt FROM USER where user_ID ="."'$param1'" ;
-   mysql_select_db('PWP3');
-   $retval = mysql_query( $sql, $conn );
-   
+   $sql = "SELECT * FROM user where id="."'$param1'" ;
+   $x = mysqli_select_db($conn, 'PWP');
+   $retval = mysqli_query($conn,$sql );
    if(! $retval ) {
        $sql2 = "INSERT INTO USER ".
-               "(user_id, user_name, user_attempt, user_score) "."VALUES ".
-               "('$param1',$param2','$param3','$param4')";
-               mysql_select_db('PWP3');
-            $retval = mysql_query( $sql2, $conn );
-   }#if user is not there in DB then insert
-   
-	$row = mysql_fetch_assoc($retval)
-      echo "{$row['user_id']}~".
-         "{$row['user_name']}~".
-         "{$row['user_attempt']}~".
-         "{$row['user_score']}~";
+               "(id, email) "."VALUES ".
+               "('$param1',$param2')";
+            $x = mysqli_select_db($conn, 'PWP');
+            $retval = mysqli_query($conn, $sql2);
+            echo "New entry made";
+            
+   }
+    else{
+	$row = mysqli_fetch_assoc($retval);
+      echo "{$row['id']}~".
+         "{$row['email']}~".
+         "{$row['sub_pd']}~".
+         "{$row['score']}~";
+    }
     #'~'will help split the string in android
-   mysql_close($conn);
+   mysqli_close($conn);
 ?>
